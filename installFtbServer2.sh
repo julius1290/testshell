4f52e88125699762c335bffee9809f4b3223d8b8
julius1290 <34373517+julius1290@users.noreply.github.com>
2020-05-01 12:31:33 +0200

    Update installFtbServer2.sh

--- installFtbServer2.sh
+++ installFtbServer2.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
-apt update && apt install zsh git wget curl
+sudo apt update && apt install zsh git wget curl
 if [ $(id -u) -eq 0 ]; then
-	username = "minecraft"
-	read -s -p "Gebe ein passwort ein : " password
-	egrep "^$username" /etc/passwd >/dev/null
-	if [ $? -eq 0 ]; then
-		echo "$username exists!"
-		exit 1
-	else
-		pass=$(perl -e 'print crypt(ARGV[0], "password")' $password)
-		useradd -m -p "$pass" "$username"
-		[ $? -eq 0 ] && echo "User has been added to system" || echo "Failed to add user"
-	fi
+        read -p "username : " username
+        read -s -p "Gebe ein passwort ein : " password
+        egrep "^$username" /etc/passwd >/dev/null
+        if [ $? -eq 0 ]; then
+                echo "$username exists!"
+                exit 1
+        else
+                pass=$(perl -e 'print crypt(ARGV[0], "password")' $password)
+                useradd -m -p "$pass" "$username"
+                [ $? -eq 0 ] && echo "User has been added to system" || echo "Failed to add user"
+        fi
 else
-	echo "Only root can run this install script"
-	exit 2
+        echo "Only root can run this install script"
+        exit 2
 fi
-cd /home/mincraft
+cd /home/$username
 mkdir server
 cd server
 wget https://media.forgecdn.net/files/2690/320/FTB+Presents+Direwolf20+1.12-1.12.2-2.5.0-Server.zip
+sudo apt update && apt install unzip
+unzip -a FTB+Presents+Direwolf20+1.12-1.12.2-2.5.0-Server.zip
+rm FTB+Presents+Direwolf20+1.12-1.12.2-2.5.0-Server.zip
+sudo apt update && apt install tmux
+sudo apt update && apt install apt-transport-https ca-certificates wget dirmngr gnupg software-properties-common
+wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
+sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
+sudo apt update && apt install adoptopenjdk-8-hotspot
+java -version
+touch eula.txt
+echo 'eula=true' > eula.txt
+cat eula.txt
+chmod +x ServerStart.sh
